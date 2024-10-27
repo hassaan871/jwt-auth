e83086c1fcde701991e5df8c3dce3de19a38cab6
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-10-27 18:17:21 +0500

    jwt token generated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const jwt = require('jsonwebtoken')
 
 const app = express()
 
@@ -8,25 +9,41 @@ const user = {
     name: "hassaan",
     password: 123
 }
+const SECRETKEY = "123"
+const options = {
+    expiresIn: '300s'
+}
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.send("Hello")
 })
 
-app.post('/login',(req, res) => {
-    if(req.body.name != user.name || req.body.password != user.password){
+app.post('/login', (req, res) => {
+    if (req.body.name != user.name || req.body.password != user.password) {
         res.json({
             "error": "Invalid username or password"
         })
-    }else{
-        res.json({
-            "success": "User found"
+    } else {
+        // res.json({
+        //     "success": "User found"
+        // })
+        jwt.sign(user, SECRETKEY, options, (err, token) => {
+            if (err) {
+                res.json({ "Error": "Error while generating the jwt, check console" })
+                console.log(`err: ${err}`);
+            }
+            res.json(
+                // { "token": token }
+                { token }
+            )
+            console.log(`token: ${token}`);
+            // console.log(typeof token);
         })
     }
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server started at port: ${PORT}`)
 })
